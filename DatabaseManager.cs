cffc775f95f6185209fa8544a20b9250c0466071
Aljianzkie <aljianruba321@gmail.com>
2026-09-24 00:10:45 +0800

    Added DatabaseManager.cs class

---
+++ DatabaseManager.cs
@@ -0,0 +1,79 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace hotel_reservation_system
+{
+    public class DatabaseManager
+    {
+        private readonly string _connectionString = "server=localhost;database=foodfestival_db;uid=root;pwd=root;port=3306";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_connectionString))
+            {
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+
+        public DataTable ExecuteQueryWithParams(string query, Dictionary<string, object> parameters)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    foreach (var param in parameters)
+                    {
+                        cmd.Parameters.AddWithValue(param.Key, param.Value);
+                    }
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+
+        public int ExecuteNonQuery(string query, Dictionary<string, object> parameters)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        foreach (var param in parameters)
+                        {
+                            cmd.Parameters.AddWithValue(param.Key, param.Value);
+                        }
+                        conn.Open();
+                        return cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Database Error: " + ex.Message);
+            }
+        }
+
+        public object ExecuteScalar(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                return cmd.ExecuteScalar();
+            }
+        }
+    }
+}
